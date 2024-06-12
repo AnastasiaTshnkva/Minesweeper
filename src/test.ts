d2fc2d436312bb7e0aedaa76b43cc253fdb0f2c3
AnastasiaTshnkva <tsuhankova.anastasia@gmail.com>
2024-06-12 23:32:07 +0300

    readonly, required, partial, omit, exclude

--- src/test.ts
+++ src/test.ts
@@ -2,21 +2,49 @@ let a: number;
 let b: boolean;
 let c: string;
 
-function login (data: unknown): {
-  const data2 = data;
-  console.log(data2);
+function login (data: unknown):void {
+  console.log(data);
 };
 
-type Permission = 'admin' | 'manager' | "user";
+interface DepartmentsOfPermissions {
+  depName: string;
+  lvl: number;
+};
+
+const DepsForPerms: Record<Permission, DepartmentsOfPermissions> = {
+  'admin': {
+    depName: 'security',
+    lvl: 10,
+  },
+  'manager': {
+    depName: 'sales',
+    lvl: 10,
+  },
+  'user' {
+    depName: 'sales',
+    lvl: 5,
+  }
+} 
+
+type Permission = 'admin' | 'manager' | 'user';
+type PermissionWithoutAdmin = Exclude<Permission, 'admin'>;
 
 type TuplePermission = [Permission, Permission];
 
-type BasicUser<A = boolean, P = TyplePermission> = {
+type BasicUser<A = boolean, P = TuplePermission> = {
   name: string;
   age: number;
   isAdmin: A;
   permissions?: P;
 };
+
+type BacisUserWithoutPermissions = Omit<BasicUser, 'permissions'>;
+
+type BasicUserWithReadonly = Readonly<BasicUser>;
+type BasicUserWithRequired = Required<BasicUser>;
+type BasicUserPartial = Partial<BasicUser>;
+
+type BasicUserReadonlyRequired = Readonly<Required<BasicUser>>;
 
 type AccountUser = {
   account: number;
@@ -38,6 +66,10 @@ let usersArray: FullUser[] = [user, user, user];
 function getFirst<T>(arr:T[]):T {
  return arr[0];
 };
+
+type basicFunc = () => string;
+
+type getFirstType = ReturnType<basicFunc>
 
 getFirst<BasicUser>(usersArray);
 
